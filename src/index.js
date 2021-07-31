5d2ebe4625f3a9f7cbc6ebe762f6a162b6fd3b17
danielcgilibert <danielcgilibert@gmail.com>
2021-08-01 01:35:37 +0200

    HtmlWebpackPlugin,CompressionPlugin...

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import  ReactDOM  from 'react-dom';
 
 ReactDOM.render(
-    <h3>Hola webpack!!!</h3>,
+    <h3>Hola webpack!</h3>,
     document.getElementById("app")
 )
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,31 @@
-module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-react", "@babel/preset-env"],
-            plugins: ["@babel/plugin-proposal-optional-chaining"],
-          },
-        },
-      },
-    ],
+const javaScriptRules = {
+  test: /\.js$/,
+  exclude: /node_modules/,
+  use: {
+    loader: "babel-loader",
+    options: {
+      presets: ["@babel/preset-react", "@babel/preset-env"],
+      plugins: ["@babel/plugin-proposal-optional-chaining"],
+    },
   },
 };
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CompressionPlugin = require("compression-webpack-plugin");
+
+module.exports = (env, {mode}) => ({
+  output: {
+    filename: 'app.[hash].js'
+  },
+  module: {
+    rules: [javaScriptRules]
+  },
+  plugins: [
+    mode === 'production' ? 
+    new CompressionPlugin() 
+    :
+    new HtmlWebpackPlugin({
+      title: 'Webpack',
+      template: 'src/index.html'
+    }),
+  ]
+});
