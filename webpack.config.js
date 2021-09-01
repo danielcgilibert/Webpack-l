827289732497ee8c0c6b7d70602028423bf8c6d1
Daniel <danielcgilibert@gmail.com>
2021-09-01 21:47:45 +0200

    Configurado hash y modo producción o desarrollo

--- webpack.config.js
+++ webpack.config.js
@@ -23,21 +23,27 @@ const rulesForStyles = {
 
 const rules = [rulesForJavaScript, rulesForStyles];
 
-module.exports = {
-  // entry: './src/index.js'
-  output: {
-    path: path.resolve(__dirname, "build"),
-  },
-  plugins: [new HtmlWebpackPlugin({ template: "src/index.html" })],
-  module: { rules },
-  devServer: {
-    //configuración devServer
-    open: true, //abre el navegador
-    port: 3000,
-    client: {
-      overlay: true,
-    }, // abrir un overlay con errores
-    compress: true, //comprime
-  },
-  devtool: "source-map", // sirve para debuggear el código
+module.exports = (env, argv) => {
+  const { mode } = argv;
+  const isProduction = mode === "production";
+
+  return {
+    // entry: './src/index.js'
+    output: {
+      filename: isProduction ? "[name].[contenthash].js" : "main.js",
+      path: path.resolve(__dirname, "build"),
+    },
+    plugins: [new HtmlWebpackPlugin({ template: "src/index.html" })],
+    module: { rules },
+    devServer: {
+      //configuración devServer
+      open: true, //abre el navegador
+      port: 3000,
+      client: {
+        overlay: true,
+      }, // abrir un overlay con errores
+      compress: true, //comprime
+    },
+    devtool: "source-map", // sirve para debuggear el código
+  };
 };
